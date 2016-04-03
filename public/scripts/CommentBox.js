f2150a0e66e8b73646c2ca18762ca5881ff98aeb
Cliff Gunn <clifton.gunn@schneider-electric.com>
2016-04-03 01:29:43 -0500

    Continuing to go through the tutorial for a real world React.js example.

---
+++ public/scripts/CommentBox.js
@@ -0,0 +1,19 @@
+/**
+ * Created by SESA300553 on 4/3/2016.
+ */
+
+// CommentBox.js
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
